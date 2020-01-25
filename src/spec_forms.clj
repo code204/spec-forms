d1377895f11de2385c6242c6ec6ed2499278535e
john-shaffer <jshaffer2112@gmail.com>
2020-01-25 15:54:38 -0600

    Allow a string message argument to validator.

--- src/spec_forms.clj
+++ src/spec_forms.clj
@@ -2,6 +2,7 @@
   (:require [clojure.alpha.spec :as s]
             [spec-forms.impl]))
 
-(defn validator [f message-fn]
+(defn validator [f message-or-fn]
   (s/resolve-spec
-   (list `validator f message-fn)))
+   (list `validator f
+         (if (fn? message-or-fn) message-or-fn (constantly message-or-fn)))))
