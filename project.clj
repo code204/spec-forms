d1d546a184342e164c4b8528bc1668883bfd2d1f
john-shaffer <jshaffer2112@gmail.com>
2020-02-15 16:16:54 -0600

    Add a project.clj for deployment to Clojars, since I couldn't get it working with deps.edn.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject code204/spec-forms "0.1.0-alpha2"
+  :url "https://github.com/code204/spec-forms"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [metosin/spec-tools "0.10.1"]])
